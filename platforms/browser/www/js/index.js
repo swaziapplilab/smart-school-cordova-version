1abc7a172e802438f43876570004605883e5ed83
emmanuel <swaziapplilab@gmail.com>
2020-03-01 20:47:15 +0200

    This is my second commit

---
+++ platforms/browser/www/js/index.js
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+
+var app = {
+	initialize : function(){
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+		
+	},
+	onDeviceReady: function(){
+		console.log('deviceready');
+	
+	document.getElementById("demo2").addEventListener("click", myFunction2);
+
+	function myFunction2() {
+  		document.getElementById("demo2").innerHTML = "LOADING.....";
+  		var url = 'http://www.eswatinismartschool.co.za/learn/index.php';
+  		var target = '_blank';
+ 		var options = "location = yes";
+
+  		window.open(url, target, options);
+
+};
+
+app.initialize();
+
+	
+	
+	
+		
+	}
+}
+
+
+
+  /*
+  window.open("https://www.w3schools.com");
+  var ref = cordova.InAppBrowser.open('http://apache.org', '_blank', 'location=yes');
+  */
